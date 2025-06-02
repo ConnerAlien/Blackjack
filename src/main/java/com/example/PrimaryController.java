829739698a2828c8d15ae739545351be7361c6d9
Powell_Conner <powec169@hsd.k12.or.us>
2025-06-02 09:31:49 -0700

    Adding scene elements (UI)

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -2,12 +2,22 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class PrimaryController {
 
+    //@FXML
+    //private void switchToSecondary() throws IOException {
+    //    App.setRoot("secondaryController");
+    //}
+
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    Button startButton = new Button();
+
+   //@FXML
+    //TitledPane 
 }
--- src/main/java/com/example/SecondaryController.java
+++ src/main/java/com/example/SecondaryController.java
@@ -2,6 +2,11 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 
 public class SecondaryController {
 
